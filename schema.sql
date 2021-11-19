ca994a2de04f4b73261b6e6b02c5717f906d07db
David Silveira <daviewave@gmail.com>
2021-11-19 03:47:14 -0500

    created database and 'USE'd it

--- schema.sql
+++ schema.sql
@@ -1,4 +1,9 @@
 -- 1. Drop the desired database if it exists then create a new one
+DROP DATABASE IF EXISTS employee_management_DB;
+CREATE DATABASE employee_management_DB;
+
 -- 2. 'use' the correct db
+USE employee_management_DB
+
 -- 3. Create tables
 -- 4. add select statements

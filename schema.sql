261151a1e0b892cf1496686b81a6ea7cd45c595e
David Silveira <daviewave@gmail.com>
2021-11-19 03:54:29 -0500

    added needed tables to schema.sql for departments, roles, and employees

--- schema.sql
+++ schema.sql
@@ -6,4 +6,28 @@ CREATE DATABASE employee_management_DB;
 USE employee_management_DB
 
 -- 3. Create tables
+-- 3.a. department table
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
+);
+-- 3.b. role within department table
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  jobTitle VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+);
+-- 3.c. employee table
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  manager_id INT,
+  role_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
+
 -- 4. add select statements

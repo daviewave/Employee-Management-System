a2f467476d8f1d2488c88defc256e36d8cb1bcb5
David Silveira <daviewave@gmail.com>
2021-11-19 04:30:36 -0500

    added connection to mysql

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
 //Dependencies
 const inquirer = require("inquirer");
 const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "dsilveira",
+  password: "Walterwhite123",
+  database: "employee_management_DB",
+});

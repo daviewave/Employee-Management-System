6234b4179b6b2cbdd27adc2a3ccbd5d0a45ca799
David Silveira <daviewave@gmail.com>
2021-11-19 04:04:01 -0500

    added seeds for the different departments within the company

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+-- DEPARTMENT SEEDS -----
+INSERT INTO department (name)
+VALUE ("Sales");
+INSERT INTO department (name)
+VALUE ("Engineering");
+INSERT INTO department (name)
+VALUE ("Finance");
+INSERT INTO department (name)
+VALUE ("Legal");

701f0a1004884f3b6a9a0fb94e633e7b6a512819
David Silveira <daviewave@gmail.com>
2021-11-19 04:21:24 -0500

    added seeds for employees

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
--- DEPARTMENT SEEDS -----
+-- DEPARTMENT SEEDS 
 INSERT INTO department (name)
 VALUE ("Sales");
 INSERT INTO department (name)
@@ -8,7 +8,7 @@ VALUE ("Finance");
 INSERT INTO department (name)
 VALUE ("Legal");
 
--- ROLE SEEDS -------
+-- ROLE SEEDS 
 -- sales roles
 INSERT INTO role (jobTitle, salary, department_id)
 VALUE ("Head Salesman", 80000, 1);
@@ -34,3 +34,22 @@ INSERT INTO role (jobTitle, salary, department_id)
 VALUE ("Lawyer", 190000, 4);
 INSERT INTO role (jobTitle, salary, department_id)
 VALUE ("Receptionist", 190000, 4);
+
+
+-- EMPLOYEE SEEDS 
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("David", "Silveira", null, 1);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Jamie", "Murray", null, 2);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("James","Ryan",null,3);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Erin", "Cambell", 1, 4);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Mohammed", "Dib", 2, 5);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Zoe", "Garcia", 3, 6);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Kayla", "Wong", 4, 7);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Sam", "Mahan", 5, 8);

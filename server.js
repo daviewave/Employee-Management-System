d35ae3d569d016f1059411af087e26fcad0147de
David Silveira <daviewave@gmail.com>
2021-11-22 03:56:43 -0500

    added switch cases for every option

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 //Dependencies
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
+const cTable = require("console.table");
+
+let employeeRole = [];
+let managers = [];
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -14,8 +18,8 @@ const connection = mysql.createConnection({
 connection.connect(function (err) {
   if (err) throw err;
   console.log("SUCCESS: CONNECTED TO PORT: 3306");
+  startEmployeeManager();
 });
-
 
 //1. Ask the user what they would like to do
 const initialPrompt = [
@@ -34,17 +38,66 @@ const initialPrompt = [
     ],
   },
 ];
+const addEmployeePrompt = [
+  {
+    type: "input",
+    name: "firstName",
+    message: "Enter their first name ",
+  },
+  {
+    type: "input",
+    name: "lastName",
+    message: "Enter their last name ",
+  },
+  {
+    type: "list",
+    name: "role",
+    message: "What is their role? ",
+    choices: chooseRole(),
+  },
+  {
+    type: "rawlist",
+    name: "choice",
+    message: "Whats their managers name?",
+    choices: chooseManager(),
+  },
+];
+
 function startEmployeeManager() {
   inquirer.prompt(initialPrompt).then((choice) => {
     switch (choice.task) {
       case "View all Employees?":
         viewAllEmployees();
         break;
+
+      case "View Employees by Role?":
+        viewEmployeeByRole();
+        break;
+
+      case "View Employees by Deparment":
+        viewEmployeeByDepartment();
+        break;
+
+      case "Add New Employee?":
+        addEmployee();
+        break;
+
+      case "Update Existing Employee?":
+        updateEmployee();
+        break;
+
+      case "Add Role?":
+        addRole();
+        break;
+
+      case "Add Department?":
+        addDepartment();
+
+      default:
+        break;
     }
   });
 }
-
-startEmployeeManager();
 
 //2. Add ability to complete each of the options listed in 1
 //view employees
@@ -59,64 +112,14 @@ function viewAllEmployees() {
 }
 
 //view employees by role
-function viewEmployeeByRole() {
-}
+function viewEmployeeByRole() {}
 
 //view employees by department
 function viewEmployeeByDepartment() {}
 //update employees
 function updateEmployee() {}
 //add employees
-
-
-
-
-const addEmployeePrompt = [
-  {
-    type: "input",
-    name: "firstName",
-    message: "Enter their first name ",
-  },
-  {
-    type: "input",
-    name: "lastname",
-    message: "Enter their last name ",
-  },
-  {
-    type: "list",
-    name: "role",
-    message: "What is their role? ",
-  },
-  {
-    type: "rawlist",
-    name: "choice",
-    message: "Whats their managers name?",
-  },
-];
-
 function addEmployee() {}
-let employeeRole = [];
-function chooseRole() {
-  connection.query("SELECT * FROM role", (err, res) => {
-    if (err) throw err;
-    for (var i = 0; i < res.length; i++) {
-      employeeRole.push(res[i].jobTitle);
-    }
-  });
-  return employeeRole;
-}
-let managers = [];
-function chooseManager() {
-  connection.query("SELECT * FROM role", (err, res) => {
-    if (err) throw err;
-    for (var i = 0; i < res.length; i++) {
-      managers.push(res[i].first_name);
-    }
-  });
-  return managers;
-}
-
-
 //add roles
 function addRole() {}
 //add departments
